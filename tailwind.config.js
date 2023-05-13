f73ca90d22eaeea824c69614e4aba61fcdbdb2d9
Vladislavs Silins <vladislavs.silins@bertelsmann.de>
2023-05-13 21:46:34 +0300

    feat: add grid demo component

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,33 +1,38 @@
-const { createGlobPatternsForDependencies } = require('@nx/angular/tailwind');
-const { join } = require('path');
-
-/** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    join(__dirname, 'src/**/!(*.stories|*.spec).{ts,html}'),
-    ...createGlobPatternsForDependencies(__dirname),
-  ],
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
   theme: {
-    colors: {
-      primary: {
-        light: '#5eead4',
-        DEFAULT: '#14b8a6',
-        dark: '#0f766e',
+    extend: {
+      spacing: {
+        24: '6rem',
+        64: '16rem',
       },
-      secondary: {
-        light: '#bae6fd',
-        DEFAULT: '#0ea5e9',
-        dark: '#0369a1',
+      colors: {
+        gray: {
+          200: '#EEEEEE',
+          300: '#E0E0E0',
+          600: '#757575',
+        },
       },
-      white: '#ffffff',
-      black: '#000000',
+      borderRadius: {
+        default: '0.25rem',
+      },
+      boxShadow: {
+        md: '0 4px 6px -1px rgba(0, 0, 0, 0.1), 0 2px 4px -1px rgba(0, 0, 0, 0.06)',
+      },
+      fontSize: {
+        bold: ['0.875rem', { lineHeight: '1.25rem' }],
+      },
+      padding: {
+        8: '2rem',
+      },
+      margin: {
+        8: '2rem',
+      },
     },
-    spacing: {
-      sm: '0.5rem',
-      md: '1rem',
-      lg: '1.5rem',
-      xl: '2rem',
-    },
+  },
+  variants: {
+    extend: {},
   },
   plugins: [],
 };
