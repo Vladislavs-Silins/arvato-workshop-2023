0aea453ba0a1ea1e3d091c7da2a5d101aa89484f
Vladislavs Silins <vladislavs.silins@bertelsmann.de>
2023-05-21 18:18:51 +0300

    feat: add menu to the view transition demo

--- src/app/components/view-transition-demo/view-transition-demo.component.ts
+++ src/app/components/view-transition-demo/view-transition-demo.component.ts
@@ -1,11 +1,18 @@
-import { Component, ViewEncapsulation } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'workshop2023-view-transition-demo',
   templateUrl: './view-transition-demo.component.html',
   styleUrls: ['./view-transition-demo.component.scss'],
-  encapsulation: ViewEncapsulation.Emulated
+  standalone: true,
+  imports: [CommonModule],
+  encapsulation: ViewEncapsulation.None,
 })
 export class ViewTransitionDemoComponent {
+  isDemo = true;
 
+  setDemo(value: boolean) {
+    this.isDemo = value;
+  }
 }
